28bf7c90b2497020c8f1c7396618de96c02eeed4
Niklas Andreasson <eaglus_@hotmail.com>
2014-04-28 09:00:37 +0200

    Move logic to builder.js

---
+++ datepicker/builders.js
@@ -0,0 +1,109 @@
+
+exports.buildYearView = buildYearView;
+function buildYearView (date) {
+  var model = this.model;
+  var months = [];
+  var month = date.clone().month(0);
+
+  for (var i = 0; i < 12; i++) {
+    months.push({ abbr: month.format('MMM'), date: month.format('YYYY-MM') });
+    month.add('months', 1);
+  }
+
+  return months;
+};
+
+
+exports.buildDecadeView = buildDecadeView;
+function buildDecadeView(date) {
+  var model = this.model;
+  var years = [];
+  var currentYear = date.year();
+  // how far into the decade are we, eg 1 year for 2011
+  var yearsIntoDecade = currentYear % 10;
+  // subract the number of years into the decade, and 1 more so for 2011 we want to start from 2009
+  var year = date.subtract('years', yearsIntoDecade + 1);
+
+  for (var i = 0; i <= 11; i++) {
+    var yearInDecade = i > 0 && i < 11;
+    years.push({ year: year.year(), inDecade: yearInDecade });
+    year.add('years', 1);
+  }
+
+  return years;
+}
+
+
+
+exports.buildMonthView = buildMonthView;
+function buildMonthView(date) {
+  var model = this.model;
+
+  var datesFromPrevMonth = addExtraDaysFromPrevMonth(date.clone());
+  var datesInCurrentMonth = getDaysInCurrentMonth(date.clone());
+  var datesFromNextMonth = addExtraDaysFromNextMonth(date.clone());
+
+  var allDates = datesFromPrevMonth.concat(datesInCurrentMonth, datesFromNextMonth);
+
+  // split all dates up
+  var weeks = [];
+
+  while (allDates.length > 0) {
+    weeks.push(allDates.splice(0, 7));
+  }
+
+  return weeks;
+
+  function getDaysInCurrentMonth(currentDate) {
+    var dates = [];
+
+    // initially set date to first day of month
+    var date = currentDate.date(1);
+    var nrDaysInMonth = currentDate.daysInMonth();
+
+    for (var i = 1; i <= nrDaysInMonth; i++) {
+      dates.push( getDateObj( date, true ) );
+      date.add('days', 1);
+    }
+
+    return dates;
+  }
+
+  function addExtraDaysFromPrevMonth(currentDate) {
+    var dates = [];
+
+    var firstDateOfMonth = currentDate.clone().date(1);
+    var firstDayOfMonth = firstDateOfMonth.day();
+
+    for (var i = 0; i < firstDayOfMonth; i++) {
+      var prevDay = firstDateOfMonth.subtract('days', 1);
+
+      dates.unshift( getDateObj( prevDay, false ) );
+    }
+
+    return dates;
+  }
+
+  function addExtraDaysFromNextMonth(currentDate) {
+    var dates = [];
+
+    var daysInMonth = currentDate.daysInMonth();
+    var lastDastDayOfMonth = currentDate.date(daysInMonth);
+
+    for (var i = lastDastDayOfMonth.day(); i < 6; i++) {
+      var nextDay = lastDastDayOfMonth.add('days', 1);
+
+      dates.push( getDateObj( nextDay, false ) );
+    }
+
+    return dates;
+  }
+
+  function getDateObj(date, isCurrentMonth) {
+    return {
+      date: date.date(),
+      fullDate: date.format('YYYY-MM-DD'),
+      thisMonth: isCurrentMonth
+    }
+  }
+}
--- datepicker/index.js
+++ datepicker/index.js
@@ -1,6 +1,7 @@
 var util = require('util');
 var moment = require('moment');
 var ViewHelpers = require('./viewHelpers').ViewHelpers;
+var builders = require('./builders');
 
 module.exports = Datepicker;
 
@@ -30,7 +31,7 @@ Datepicker.prototype.monthView = function(date) {
   if ( !date ) return;
 
   var date = moment(date);
-  var weeks = this.buildMonthView(date.year(), date.month());
+  var weeks = builders.buildMonthView(date);
 
   model.set('weeks', weeks);
   model.set('view', 'month');
@@ -45,23 +46,10 @@ Datepicker.prototype.gotoYearView = function(year) {
 
 Datepicker.prototype.yearView = function(date) {
   var model = this.model;
-  var months = this.buildYearView(date);
+  var months = builders.buildYearView(date);
 
   model.set('months', months);
   model.set('view', 'year');
-};
-
-Datepicker.prototype.buildYearView = function(date) {
-  var model = this.model;
-  var months = [];
-  var month = date.clone().month(0);
-
-  for (var i = 0; i < 12; i++) {
-    months.push({ abbr: month.format('MMM'), date: month.format('YYYY-MM') });
-    month.add('months', 1);
-  }
-
-  return months;
 };
 
 Datepicker.prototype.nextYear = function() {
@@ -91,28 +79,10 @@ Datepicker.prototype.gotoDecadeView = function(date) {
 
 Datepicker.prototype.decadeView = function(date) {
   var model = this.model;
-  var years = this.buildDecadeView(date);
+  var years = builders.buildDecadeView(date);
 
   model.set('years', years);
   model.set('view', 'decade');
-};
-
-Datepicker.prototype.buildDecadeView = function(date) {
-  var model = this.model;
-  var years = [];
-  var currentYear = date.year();
-  // how far into the decade are we, eg 1 year for 2011
-  var yearsIntoDecade = currentYear % 10;
-  // subract the number of years into the decade, and 1 more so for 2011 we want to start from 2009
-  var year = date.subtract('years', yearsIntoDecade + 1);
-
-  for (var i = 0; i <= 11; i++) {
-    var yearInDecade = i > 0 && i < 11;
-    years.push({ year: year.year(), inDecade: yearInDecade });
-    year.add('years', 1);
-  }
-
-  return years;
 };
 
 Datepicker.prototype.prevDecade = function() {
@@ -163,80 +133,6 @@ Datepicker.prototype.nextMonth = function() {
   var nextMonthDate = currentDate.add('months', 1);
 
   this.gotoMonthView(nextMonthDate);
-};
-
-Datepicker.prototype.buildMonthView = function(year, month) {
-  var model = this.model;
-
-  var currentDate = moment({ year: year, month: month });
-
-  var datesFromPrevMonth = addExtraDaysFromPrevMonth(currentDate.clone());
-  var datesInCurrentMonth = getDaysInCurrentMonth(currentDate.clone());
-  var datesFromNextMonth = addExtraDaysFromNextMonth(currentDate.clone());
-
-  var allDates = datesFromPrevMonth.concat(datesInCurrentMonth, datesFromNextMonth);
-
-  // split all dates up
-  var weeks = [];
-
-  while (allDates.length > 0) {
-    weeks.push(allDates.splice(0, 7));
-  }
-
-  return weeks;
-
-  function getDaysInCurrentMonth(currentDate) {
-    var dates = [];
-
-    // initially set date to first day of month
-    var date = currentDate.date(1);
-    var nrDaysInMonth = currentDate.daysInMonth();
-
-    for (var i = 1; i <= nrDaysInMonth; i++) {
-      dates.push( getDateObj( date, true ) );
-      date.add('days', 1);
-    }
-
-    return dates;
-  }
-
-  function addExtraDaysFromPrevMonth(currentDate) {
-    var dates = [];
-
-    var firstDateOfMonth = currentDate.clone().date(1);
-    var firstDayOfMonth = firstDateOfMonth.day();
-
-    for (var i = 0; i < firstDayOfMonth; i++) {
-      var prevDay = firstDateOfMonth.subtract('days', 1);
-
-      dates.unshift( getDateObj( prevDay, false ) );
-    }
-
-    return dates;
-  }
-
-  function addExtraDaysFromNextMonth(currentDate) {
-    var dates = [];
-
-    var daysInMonth = currentDate.daysInMonth();
-    var lastDastDayOfMonth = currentDate.date(daysInMonth);
-
-    for (var i = lastDastDayOfMonth.day(); i < 6; i++) {
-      var nextDay = lastDastDayOfMonth.add('days', 1);
-
-      dates.push( getDateObj( nextDay, false ) );
-    }
-
-    return dates;
-  }
-
-  function getDateObj(date, isCurrentMonth) {
-    return {
-      date: date.date(),
-      fullDate: date.format('YYYY-MM-DD'),
-      thisMonth: isCurrentMonth
-    }
-  }
 };
 
 Datepicker.prototype.setCurrentDate = function(currentDate) {
